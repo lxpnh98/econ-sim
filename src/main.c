fabcc2ab7a4071dab94bc36cc28c5ef28e238347
Alexandre Pinho <lxpnh98@gmail.com>
2017-06-15 20:48:48 +0100

    Complete update_market().

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 
 #define MAX2(A, B) ((A > B) ? A : B)
 
+#define MIN2(A, B) ((A < B) ? A : B)
+
 #define NUM_GOODS           4
 
 #define MAX_AGENTS          100
@@ -11,6 +13,10 @@
 #define MAX_REQUESTS        500
 
 #define MAX_FOOD            50
+
+#define TRADE_SUCCESS       1
+
+#define TRADE_FAILURE       0
 
 typedef enum {
     WOODWORKER,
@@ -55,9 +61,9 @@ typedef struct {
 typedef struct {
     Agent agents[MAX_AGENTS];
     int num_bids;
-    Bid bids[MAX_REQUESTS];
+    Bid *bids[MAX_REQUESTS];
     int num_asks;
-    Ask asks[MAX_REQUESTS];
+    Ask *asks[MAX_REQUESTS];
     float mean;
 } Market;
 
@@ -117,10 +123,11 @@ void perform_production(Agent *a)
 void create_bid(Market *m, Agent *a, Good g, float price, float quantity)
 {
     if (m->num_bids < MAX_REQUESTS) {
-        m->bids[m->num_bids].agent = a;
-        m->bids[m->num_bids].good = g;
-        m->bids[m->num_bids].price = price;
-        m->bids[m->num_bids].quantity = quantity;
+        m->bids[m->num_bids] = (Bid *)malloc(sizeof(Bid));
+        m->bids[m->num_bids]->agent = a;
+        m->bids[m->num_bids]->good = g;
+        m->bids[m->num_bids]->price = price;
+        m->bids[m->num_bids]->quantity = quantity;
         m->num_bids++;
     }
 }
@@ -137,10 +144,11 @@ void create_bids(Market *m, Agent *a)
 void create_ask(Market *m, Agent *a, Good g, float price, float quantity)
 {
     if (m->num_asks < MAX_REQUESTS) {
-        m->asks[m->num_asks].agent = a;
-        m->asks[m->num_asks].good = g;
-        m->asks[m->num_asks].price = price;
-        m->asks[m->num_asks].quantity = quantity;
+        m->asks[m->num_asks] = (Ask *)malloc(sizeof(Ask));
+        m->asks[m->num_asks]->agent = a;
+        m->asks[m->num_asks]->good = g;
+        m->asks[m->num_asks]->price = price;
+        m->asks[m->num_asks]->quantity = quantity;
         m->num_asks++;
     }
 }
@@ -159,13 +167,73 @@ int cmp_offer(const void *o1, const void *o2)
     return (((Bid *)o1)->price < ((Bid *)o1)->price) - ((Bid *)o1)->price > ((Bid *)o2)->price;
 }
 
+void remove_top_bid(Market *m)
+{
+    int i;
+    for (i = 0; i < m->num_bids - 1; i++) {
+        m->bids[i] = m->bids[i + 1];
+    }
+    m->num_bids--;
+}
+
+void remove_top_ask(Market *m)
+{
+    int i;
+    for (i = 0; i < m->num_asks - 1; i++) {
+        m->asks[i] = m->asks[i + 1];
+    }
+    m->num_asks--;
+}
+
+void update_price_model(Agent *a, float price, int success)
+{
+}
+
 void resolve_offers(Market *m)
 {
+    float quantity;
+    float price;
+    Bid *top_bid;
+    Ask *top_ask;
     qsort(m->bids, m->num_bids, sizeof(Bid), cmp_offer); 
+    qsort(m->asks, m->num_asks, sizeof(Ask), cmp_offer); 
     while (m->num_bids > 0 && m->num_asks > 0) {
-        // Match top bid and top ask
+        // Set up trade
+        top_bid = m->bids[0];
+        top_ask = m->asks[0];
+        quantity = MIN2(top_bid->quantity, top_ask->quantity);
+        price = (top_bid->price + top_ask->price) / 2.0;
+
+        // Exchange
+        top_bid->quantity -= quantity;
+        top_ask->quantity -= quantity;
+        top_bid->agent->good_quantity[top_bid->good] += quantity;
+        top_ask->agent->good_quantity[top_ask->good] -= quantity;
+        top_bid->agent->currency -= price * quantity;
+        top_ask->agent->currency += price * quantity;
+
+        // Update price models
+        update_price_model(top_bid->agent, price, TRADE_SUCCESS);
+        update_price_model(top_ask->agent, price, TRADE_SUCCESS);
+
+        if (top_bid->quantity == 0) {
+            remove_top_bid(m);
+        }
+        if (top_ask->quantity == 0) {
+            remove_top_ask(m);
+        }
     }
+
     // Deal with failed offers
+    while (m->num_bids > 0) {
+        update_price_model(top_bid->agent, price, TRADE_FAILURE);
+        remove_top_bid(m);
+    }
+
+    while (m->num_asks > 0) {
+        update_price_model(top_bid->agent, price, TRADE_FAILURE);
+        remove_top_ask(m);
+    }
 }
 
 void update_market(Market *m)
