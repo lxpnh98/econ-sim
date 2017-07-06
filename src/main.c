ba89587b7b2d373a69f124248a4a723b229b9946
Alexandre Pinho <lxpnh98@gmail.com>
2017-07-06 15:48:07 +0100

    Add basic command line interface.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "market.h"
 #include "market_init.h"
 #include "display.h"
 
+#define MAX_BUFFER                  1024
+
 #define ROUNDS                      5
 
 #define SEED                        1
 
-int main()
+void run_sim()
 {
     int n;
     Market m;
@@ -18,10 +21,35 @@ int main()
     print_market_info(&m, 1);
     for (n = 0; n < ROUNDS; n++) {
         update_market(&m);
-        //print_market_info(&m);
         printf("\n---------------\n");
         printf("Round complete.");
         printf("\n---------------\n\n");
+    }
+}
+
+int main()
+{
+    int tokens, i;
+    char command[MAX_BUFFER];
+    char *args[MAX_BUFFER];
+    while (1) {
+        printf("> ");
+        if(fgets(command, MAX_BUFFER, stdin) != NULL) {
+            i = 0;
+            args[i++] = strtok(command, " ");
+            while ((args[i++] = strtok(NULL, " ")));
+            tokens = i - 1;
+            if (tokens == 1) {
+                for (i = 0; (*args)[i] != '\0'; i++);
+                (*args)[i - 1] = '\0';
+            }
+            for (i = 0; i < tokens; i++) {
+                if (strcmp(args[i], "run") == 0)
+                    run_sim();
+                if (strcmp(args[i], "exit") == 0)
+                    exit(0);
+            }
+        }
     }
     return EXIT_SUCCESS;
 }
